68abd942f27a51e625f3294d820af220b37b1108
Abhishek-coder2001 <abhishek800.roy@gmail.com>
2024-07-19 09:21:15 +0530

    something create

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +14,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <div className="min-h-screen   bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+          {" "}
+          {children}
+        </div>
+
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ app/service/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      Sercvice page
+    </div>
+  )
+}
+
+export default page
